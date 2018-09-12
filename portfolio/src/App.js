0e20c42af7ae5b98c0c04912f9bde819cb8eaf73
ckopecky <ckopecky@github.com>
2018-09-12 13:40:24 -0700

    Add ability to switch between a light and dark theme

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,19 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import sun from './img/sun.png';
+import moon from './img/moon.png';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDay: true,
+      theme: "body-light"
+    };
+    
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange() {
+    const isDay = !this.state.isDay;
+
+    this.setState({isDay: isDay,
+      theme: isDay ? "body-light" : "body-dark" })
+  }
+  render () {
+    console.log(this.state.theme)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className={this.state.theme} >
+          <div><img className="time-of-day-icon"src={sun} alt="sun"/></div>
+          <label className="switch">
+            <input type="checkbox" value={this.state.isDay} onChange={this.handleChange} />
+            <div className="slider"></div>
+          </label>
+          <div><img className="time-of-day-icon"src={moon} alt="moon"/></div>
+        </div>
     );
   }
 }
