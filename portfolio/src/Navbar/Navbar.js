3c4e506c4264de652c9216211f74aec0630df222
ckopecky <ckopecky@github.com>
2018-09-12 18:36:53 -0700

    Create navbar

---
+++ portfolio/src/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Navbar.css";
+const Navbar = () => {
+    return (
+        <div className="navbar-wrapper">
+            <a href="#">About Me</a>
+            <a href="#">Contact Me</a>
+            <a href="#">Resumé</a>
+            <a href="#">Portfolio</a>
+
+        </div>
+    );
+};
+
+export default Navbar;
