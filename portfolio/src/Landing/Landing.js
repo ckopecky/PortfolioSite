e6eb24167128df7c1321d83329237200db2d0e84
ckopecky <ckopecky@github.com>
2018-09-20 02:04:29 -0700

    Add Landing page. Animation with my name
    Thought is to add second line with about me titles
    
    Then a third line that allows person to enter site.

---
+++ portfolio/src/Landing/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = (props) => {
+  return (
+    <div className="landing-container">
+      <div className="text">Christina Kopecky</div>
+    </div>
+  )
+}
+
+export default Landing;
