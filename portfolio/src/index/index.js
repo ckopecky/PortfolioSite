18af8335220c68149cfbac17f07721892303d4a8
ckopecky <ckopecky@github.com>
2018-10-29 19:04:05 -0700

    Starting from scratch
    Started a README with UX ideation and wireframes

--- portfolio/src/index/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
-import './index.css';
-import App from './App/App';
-import registerServiceWorker from './index/registerServiceWorker';
-
-ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
-registerServiceWorker();
