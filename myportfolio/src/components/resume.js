6454a72f62dd7333ed58b130bf8f7829cbceced9
ckopecky <ckopecky@github.com>
2018-11-01 13:22:47 -0700

    Resume - PDF download button added. Contact Me button added
    With styling
    
    Took out :id on projects link bc throwing err

--- myportfolio/src/components/resume.js
+++ myportfolio/src/components/resume.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import Resume from '../img/Kopecky_Christina_Resume.pdf';
 class resume extends Component {
     render() {
         return (
             <div className="switch">
                 <div className="resume-container">
-                    <div className="resume-card"></div>
+                    <div className="resume-button-container">
+                        <div className="download-pdf"><a className="resume-link" href={Resume} target="_blank" rel="noopener noreferrer" >
+                            Download PDF</a>
+                        </div>
+                        <div className="download-pdf">
+                            <Link className="resume-link" to="./contact">Contact Me</Link>
+                        </div>
+                    </div>
+                    <div className="resume-card">Test 123</div>
+
                 </div>
             </div>
         );
