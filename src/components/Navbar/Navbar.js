5790005a178281eaa1353378892e4e7a65a07ef0
ckopecky <kopecky12112@gmail.com>
2019-04-06 21:18:28 -0700

    Add LESS to project
    Create working responsive navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { twitterUrl, githubUrl, linkedInUrl } from '../../services/resourceURLS';
+import { Nav, NavbarBrand, Collapse, NavbarToggler, NavItem, Navbar, NavLink } from 'reactstrap';
+
+import '../../css/index.css';
+
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+          collapsed: true
+        };
+      }
+    
+      toggleNavbar() {
+        this.setState({
+          collapsed: !this.state.collapsed
+        });
+      }
+      render() {
+        return (
+            <div>
+                <Navbar className="nav-bar-portfolio" color="faded" light expand="md">
+                    <NavbarBrand href="/" className="mr-auto nav-bar-brand">
+                        <span>Christina Kopecky</span>
+                        <h1 className="hidden">CK</h1>
+                    </NavbarBrand>
+                    <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                    <Collapse isOpen={!this.state.collapsed} navbar>
+                    <Nav className="nav-bar-links" navbar>
+                        <div id="collapsed-nav-link-bar" className="nav-link-bar">
+                            <NavItem>
+                                <NavLink href="/components/">Portfolio</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/components/">Resume</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/components/">Blog</NavLink>
+                            </NavItem>
+                        </div>
+                        <NavItem className="career-urls">
+                        <NavLink href={twitterUrl}>
+                            <i class="fab fa-twitter"></i></NavLink>
+                        <NavLink href={githubUrl}>
+                            <i class="fab fa-github"></i></NavLink>
+                        <NavLink href={linkedInUrl}>
+                            <i class="fab fa-linkedin"></i></NavLink>
+                    </NavItem>
+                    </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+      }
+    }
+
+export default Navigation;
