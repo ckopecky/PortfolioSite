a9ba6610048f7c37f5cda6288a3c469ac4209dbe
ckopecky <kopecky12112@gmail.com>
2019-04-06 21:21:10 -0700

    Update paths and add bootstrap css file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+require('dotenv').config();
+
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
