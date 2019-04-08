c3e42ba8f0bbca185cc0e3bff727586eb98aecd4
ckopecky <kopecky12112@gmail.com>
2019-04-08 01:44:39 -0700

    Add logic to toggle between dark/light theme

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,63 +1,89 @@
-import React from 'react';
-import { twitterUrl, githubUrl, linkedInUrl } from '../../services/resourceURLS';
-import { Nav, NavbarBrand, Collapse, NavbarToggler, NavItem, Navbar, NavLink } from 'reactstrap';
+import React from "react";
+import {
+  twitterUrl,
+  githubUrl,
+  linkedInUrl
+} from "../../services/resourceURLS";
+import {
+  Nav,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  NavItem,
+  Navbar,
+  NavLink
+} from "reactstrap";
 
-import '../../css/index.css';
+import "../../css/index.css";
+import DarkLightSwitch from "./DarkLightSwitch";
 
 class Navigation extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.toggleNavbar = this.toggleNavbar.bind(this);
-        this.state = {
-          collapsed: true
-        };
-      }
-    
-      toggleNavbar() {
-        this.setState({
-          collapsed: !this.state.collapsed
-        });
-      }
-      render() {
-        return (
-            <div>
-                <Navbar className="nav-bar-portfolio" color="light" light expand="md">
-                    <NavbarBrand href="/" className="mr-auto nav-bar-brand">
-                        <span className="name-slug">Christina Kopecky</span>
-                        <span className="mobile-name-slug">Christina K.</span>
-                    </NavbarBrand>
-                    <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-                    <Collapse isOpen={!this.state.collapsed} navbar>
-                    <Nav className="nav-bar-links" navbar>
-                        <div id="collapsed-nav-link-bar" className="nav-link-bar">
-                            <NavItem>
-                                <NavLink href="/portfolio">Portfolio</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/resume">Resume</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/blog">Blog</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/contact">Contact</NavLink>
-                            </NavItem>
-                        </div>
-                        <NavItem className="career-urls">
-                        <NavLink href={twitterUrl}>
-                            <i class="fab fa-twitter"></i></NavLink>
-                        <NavLink href={githubUrl}>
-                            <i class="fab fa-github"></i></NavLink>
-                        <NavLink href={linkedInUrl}>
-                            <i class="fab fa-linkedin"></i></NavLink>
-                    </NavItem>
-                    </Nav>
-                    </Collapse>
-                </Navbar>
-            </div>
-        );
-      }
-    }
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+  render() {
+    console.log(this.props, "props from nav");
+    return (
+      <div>
+        <Navbar className="nav-bar-portfolio" expand="md">
+          <NavbarBrand href="/" className="mr-auto nav-bar-brand">
+            <span className="name-slug">Christina Kopecky</span>
+            <span className="mobile-name-slug">Christina K.</span>
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2">
+            <i className="fas fa-bars" />
+          </NavbarToggler>
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav className="nav-bar-links" navbar>
+              <div id="collapsed-nav-link-bar" className="nav-link-bar">
+                <NavItem>
+                  <NavLink href="/portfolio">Portfolio</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/resume">Resume</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/blog">Blog</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/contact">Contact</NavLink>
+                </NavItem>
+              </div>
+              <NavItem className="career-urls">
+                <NavLink href={twitterUrl}>
+                  <i className="fab fa-twitter" />
+                </NavLink>
+                <NavLink href={githubUrl}>
+                  <i className="fab fa-github" />
+                </NavLink>
+                <NavLink href={linkedInUrl}>
+                  <i className="fab fa-linkedin" />
+                </NavLink>
+              </NavItem>
+              
+            </Nav>
+            <DarkLightSwitch
+                toggleDarkLight={this.props.toggleDarkLight}
+                day={this.props.day}
+                buttonLabel={this.props.buttonLabel}
+            />
+          </Collapse>
+          
+        </Navbar>
+      </div>
+    );
+  }
+}
 
 export default Navigation;
