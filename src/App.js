f1a57006646db94458447b3c3816713d0f7a1c4e
ckopecky <kopecky12112@gmail.com>
2019-04-07 21:55:10 -0700

    Landing Hero placeholder created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './css/index.css';
 // import { Route } from 'react-router-dom'
 import Navigation from './components/Navbar/Navbar';
+import LandingHero from './components/Landing-Hero';
 class App extends Component {
     
   render() {
     return (
       <div className="App">
         <Navigation />
+        <LandingHero />
       </div>
     );
   }
