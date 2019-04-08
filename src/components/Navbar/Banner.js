291ee35fdcfbfbfc05f9ea0f11db06d68e0d55d1
ckopecky <kopecky12112@gmail.com>
2019-04-08 15:34:38 -0700

    Add a bit of blur effect on images so that I can write over them.
    Test mode changes on banner container with diff background colors

--- src/components/Navbar/Banner.js
+++ src/components/Navbar/Banner.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Banner = () => {
     return (
         <div className="banner-container">
-            
+            <div className="text-container">
+                Text container
+            </div>
         </div>
     );
 };
