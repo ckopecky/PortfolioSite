3fb764299ba52b6c5a14a228a0054745212e5d33
ckopecky <kopecky12112@gmail.com>
2019-04-08 11:36:52 -0700

    Scaffold of Project

---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Landing = (props) => {
+    return (
+        <div style={{height: '100vh'}} className="landing-container">
+        Plan of Attack for Landing Page:<br/>
+            >Banner <br/>
+            >My Experience <br/>
+            >See Some of My Recent Work: <br/>
+            >Contact Me! <br/>
+        </div>
+    );
+};
+
+export default Landing;
---
+++ src/components/Resume.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div className="container">
+            Plan of attack for Resume  page:
+
+            > mark up resume and post <br/>
+            > Provide ability to download resume in PDF format<br/>
+
+        </div>
+    );
+};
+
+export default Resume;
