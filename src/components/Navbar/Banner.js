59637dd2433364fbd342c4240a801214e726ff2a
ckopecky <kopecky12112@gmail.com>
2019-04-08 15:55:14 -0700

    Add profile pic

--- src/components/Navbar/Banner.js
+++ src/components/Navbar/Banner.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ProfilePic from "../../img/profilepic.jpg";
 
 const Banner = () => {
     return (
         <div className="banner-container">
+            <img src={ProfilePic} alt="Christina Kopecky Profile"/>
             <div className="text-container">
                 Text container
             </div>
