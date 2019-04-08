8facb4c72ed31f02de73c0ca66a06b7768bd3936
ckopecky <kopecky12112@gmail.com>
2019-04-08 11:36:52 -0700

    Scaffold of Project

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="container">
+            Plan of Attack for Contact:
+
+            checkout netlify forms <br/>
+            or use formspree.io to send information to me. <br/>
+            > Required information: Name, Reason for Contact, Message <br/>
+            > Recaptcha if not included in package <br/>
+
+        </div>
+    );
+};
+
+export default Contact;
