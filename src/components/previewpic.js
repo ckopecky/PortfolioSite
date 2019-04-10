f87ce50161672f38cd78d92e2c315818c3c47a69
ckopecky <kopecky12112@gmail.com>
2019-04-10 14:34:26 -0700

    Add previewpic

---
+++ src/components/previewpic.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Preview from '../img/Website-Preview.png';
+
+class previewpic extends React.Component {
+    
+    componentDidMount() {
+        if(window.localStorage.Theme) {
+            document.documentElement.setAttribute("data-theme", window.localStorage.Theme);
+        }
+    }
+
+    render() {
+        return (
+            <img style={{width: "100%"}} src={Preview} alt="website-preview"/>
+        );
+    }
+    
+};
+
+export default previewpic;
