499d73b1e5929f0ab021278eff180ea095f1eb79
ckopecky <kopecky12112@gmail.com>
2019-04-12 14:05:24 -0700

    Add ScrollToTop feature so that long pages will go to top of page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
+import ScrollToTop from './ScrollToTop';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 require('dotenv').config();
 
 
-ReactDOM.render(<Router><App className="App"/></Router>, document.getElementById('root'));
+ReactDOM.render(
+<Router>
+    <ScrollToTop>
+        <App className="App"/>
+    </ScrollToTop>
+</Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
