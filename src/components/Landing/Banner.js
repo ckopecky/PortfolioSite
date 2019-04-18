4a9ca833c49dbbff1949f620a43392b6a018f6e0
ckopecky <kopecky12112@gmail.com>
2019-04-18 04:07:41 -0700

    Move external links to hero

--- src/components/Landing/Banner.js
+++ src/components/Landing/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ProfilePic from "../../img/WebsiteAssets/profilepic.jpg";
+import { githubUrl, linkedInUrl } from '../../services/resourceURLS';
 
 const Banner = () => {
     return (
@@ -10,7 +11,7 @@ const Banner = () => {
                         <img src={ProfilePic} alt="circle-profile"/>
                         <div className="welcome-intro">
                             <h1><span className="carat">></span> Hi! I'm Christina Kopecky.</h1>
-                            <p><span className="carat">></span> I'm a full-stack web developer, professional flutist and flute teacher that lives in San Jose, California. I started studying Full-Stack Web Development, UX, and Computer Science in 2018 with the hope that I can change the focus of my career! </p>
+                            <p><span className="carat">></span> I'm a full-stack web developer, professional flutist and flute teacher that lives in San Jose, California. I started studying Full-Stack Web Development, UX, and Computer Science in 2018 to change the focus of my career! Welcome!</p>
                             <br/>
                         </div>
                     </div>
@@ -21,7 +22,18 @@ const Banner = () => {
                         <p><span className="carat">></span> My goal is to become a full-stack software engineer. I love interacting with designers and other engineers to work toward a common goal!</p>
                     </div>
                     <h3><span className="carat">></span> Welcome!</h3> */}
-                    <Link to="/portfolio"><button>My Recent Projects</button></Link>
+                    <div className="banner-button-container">
+                        <Link to="/portfolio"><button>My Recent Projects</button></Link>
+                        <Link to="/contact"><button>Contact Me</button></Link>
+                    </div>
+                    <div className="career-urls">
+                        <a href={githubUrl}>
+                            <i className="fab fa-github" />
+                        </a>
+                        <a href={linkedInUrl}>
+                            <i className="fab fa-linkedin" />
+                        </a>
+                    </div>
                 </div>
             </div>
     );
