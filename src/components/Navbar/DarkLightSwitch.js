2699b2e8d53159c77a410b8566f290d7713e06f5
ckopecky <kopecky12112@gmail.com>
2019-04-18 04:07:50 -0700

    Remove console log

--- src/components/Navbar/DarkLightSwitch.js
+++ src/components/Navbar/DarkLightSwitch.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const DarkLightSwitch = (props) => {
-    // console.log(props, "props");
     return (
         <div className="day-switch">
             <div htmlFor="dark_light" 
