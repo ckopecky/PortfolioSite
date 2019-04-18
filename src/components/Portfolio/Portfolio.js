46591fb662fb2edf9e0e95431f4c29c883aa3140
ckopecky <kopecky12112@gmail.com>
2019-04-18 04:08:29 -0700

    remove console log

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -19,7 +19,6 @@ class Portfolio extends React.Component {
     }
 
     render(){
-        console.log(this.state.projects);
         return (
             <CardDeck className="main-content">
                 {this.state.projects.map(project => {
