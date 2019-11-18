b9871029e93c28b86c7720662c6a1ff32d71ca85
ckopecky <kopecky12112@gmail.com>
2019-11-17 16:51:23 -0800

    Update node and take out buggy code

--- src/App.js
+++ src/App.js
@@ -45,19 +45,6 @@ class App extends Component {
   }
 
   render() {
-    let host = window.location.host;
-    let protocol = window.location.protocol;
-    let parts = host.split(".");
-    let subdomain = "";
-    // If we get more than 3 parts, then we have a subdomain
-    // INFO: This could be 4, if you have a co.uk TLD or something like that.
-    if (parts.length >= 3) {
-      subdomain = parts[0];
-      // Remove the subdomain from the parts list
-      parts.splice(0, 1);
-      // Set the location to the new url
-      window.location = protocol + "//" + parts.join(".") + "/" + subdomain;
-    }
     return (
       <>
       <div id="body">
